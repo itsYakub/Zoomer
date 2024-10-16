b9252d57dc39d1c9ebd89acac915965a1a3696df
Jakub Oleksiak <yakubofficialmail@gmail.com>
2024-10-16 13:23:17 +0200

    Rendering
    Added some basic fundamentals for rendering stuff onto the screen; TODO: Render batching maybe?

--- main.c
+++ main.c
@@ -7,6 +7,8 @@
 
 #include "SDL2/SDL.h"
 #include "glad/glad.h"
+#include "cglm/types.h"
+#include "cglm/cam.h"
 
 // TODO(yakub):
 // 1. Create a functionality for storing the display capture in the char* array
@@ -21,22 +23,32 @@ const char* glsl_vert =
 "layout (location = 0) in vec3 a_Pos;\n"
 "layout (location = 1) in vec4 a_Col;\n"
 "layout (location = 2) in vec2 a_TexCoord;\n"
+"layout (location = 3) in float a_TexId;\n"
 "out vec4 v_Col;\n"
 "out vec2 v_TexCoord;\n"
+"out float v_TexId;\n"
+"uniform mat4 u_proj;\n"
+"uniform mat4 u_view;\n"
 "void main() {\n"
-" 	gl_Position = vec4(a_Pos, 1.0f);\n"
+" 	gl_Position = u_proj * u_view * vec4(a_Pos, 1.0f);\n"
 "	v_Col = a_Col;\n"
 "	v_TexCoord = a_TexCoord;\n"
+"	v_TexId = a_TexId;\n"
 "}";
 
 const char* glsl_frag =
 "#version 460 core\n"
 "in vec4 v_Col;\n"
 "in vec2 v_TexCoord;\n"
+"in float v_TexId;\n"
 "out vec4 f_Col;\n"
 "uniform sampler2D u_Texture;\n"
 "void main() {\n"
-"	f_Col = texture(u_Texture, v_TexCoord) * v_Col;\n"
+"   int f_TexId = int(v_TexId);"
+"   if(f_TexId != 0)\n"
+"	    f_Col = texture(u_Texture, v_TexCoord) * v_Col;\n"
+"   else\n"
+"       f_Col = v_Col;\n"
 "}";
 
 typedef struct s_tex2d {
@@ -46,79 +58,106 @@ typedef struct s_tex2d {
     unsigned int id;
 } t_tex2d;
 
-char* ft_screen_capture(int* w, int* h);
-t_tex2d ft_tex2d_screen_capture(int w, int h, char* data);
+typedef struct s_core {
+    void* window;
+    SDL_GLContext context;
+    int exit;
 
-int ft_draw_tex2d(t_tex2d tex, float position[2], float size[2]);
+    unsigned int sh_prog;
+
+} t_core;
+
+static t_core CORE;
+
+int ft_init(unsigned int w, unsigned int h, const char* title);
+int ft_init_window(unsigned int w, unsigned int h, const char* title);
+int ft_init_opengl(void);
+
+int ft_poll_events(void);
+int ft_should_quit(void);
+int ft_display(void);
+int ft_quit(void);
+
+char* ft_screen_capture(int w, int h);
+t_tex2d ft_tex2d(int w, int h, char* data);
+
+int ft_draw_rect(vec2 position, vec2 size, vec4 color);
+int ft_draw_tex2d(t_tex2d tex, vec2 position, vec2 size);
 
 int main(int argc, const char* argv[]) {
-    unsigned int capture_width = 0;
-    unsigned int capture_height = 0;
-    char* capture_data = ft_screen_capture(&capture_width, &capture_height);
+    unsigned int capture_width = 1920;
+    unsigned int capture_height = 1080;
+    char* capture_data = ft_screen_capture(capture_width, capture_height);
 
+    ft_init(capture_width, capture_height, "Okay, Zoomer | 1.0.0"); 
+
+    t_tex2d capture_texture = ft_tex2d(capture_width, capture_height, capture_data);
+
+    mat4 mat_proj = GLM_MAT4_IDENTITY_INIT;
+    mat4 mat_view = GLM_MAT4_IDENTITY_INIT;
+
+    glm_ortho(0.0f, capture_width, capture_height, 0.0f, -1.0f, 1.0f, mat_proj);
+    glUniformMatrix4fv(glGetUniformLocation(CORE.sh_prog, "u_proj"), 1, GL_FALSE, &mat_proj[0][0]);
+
+	while(!ft_should_quit()) {
+	    ft_poll_events();	
+
+		glClear(GL_COLOR_BUFFER_BIT);
+		glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+
+        glUniformMatrix4fv(glGetUniformLocation(CORE.sh_prog, "u_view"), 1, GL_FALSE, &mat_view[0][0]);
+
+        ft_draw_tex2d(capture_texture, (vec2) { 0.0f, 0.0f }, (vec2) { capture_width, capture_height });
+
+        ft_display();
+	}
+
+    free(capture_data);
+    glDeleteTextures(1, &capture_texture.id);
+
+    ft_quit();
+
+	return 0;
+}
+
+int ft_init(unsigned int w, unsigned int h, const char* title) {
+    ft_init_window(w, h, title);
+    ft_init_opengl();
+
+    return 1;
+}
+
+
+int ft_init_window(unsigned int w, unsigned int h, const char* title) {
 	if(SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-		return 1;
+		return 0;
 	}
 
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
 
-	SDL_Window* window = SDL_CreateWindow(
-		"Hello, OpenGL!",
+	CORE.window = SDL_CreateWindow(
+		title,
 		SDL_WINDOWPOS_CENTERED,
 		SDL_WINDOWPOS_CENTERED,
-		1920,
-		1080,
-		SDL_WINDOW_OPENGL | SDL_WINDOW_FULLSCREEN
+		w,
+	    h,
+		SDL_WINDOW_OPENGL | SDL_WINDOW_FULLSCREEN | SDL_WINDOW_BORDERLESS
 	);
 
-	SDL_GLContext context = SDL_GL_CreateContext(window);
-	SDL_GL_MakeCurrent(window, context);
+	CORE.context = SDL_GL_CreateContext(CORE.window);
+	SDL_GL_MakeCurrent(CORE.window, CORE.context);
 	SDL_GL_SetSwapInterval(1);
 
 	gladLoadGL();
 
-    // SECTION: Buffer objects
+    glViewport(0, 0, w, h);
 
-    GLuint vert_arr;
-    glGenVertexArrays(1, &vert_arr);
-    glBindVertexArray(vert_arr);
+    return 1;
+}
 
-    GLuint vert_buf;
-    glGenBuffers(1, &vert_buf);
-    glBindBuffer(GL_ARRAY_BUFFER, vert_buf);
-
-    GLuint elem_buf;
-    glGenBuffers(1, &elem_buf);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elem_buf);
-
-    GLfloat vertices[] = {
-        -1.0f, -1.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
-         1.0f, -1.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
-        -1.0f,  1.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
-         1.0f,  1.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
-    };
-
-    GLuint indices[] = {
-        0, 1, 2,
-        1, 2, 3
-    };
-
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 9 * sizeof(GLfloat), (void*) (0 * sizeof(GLfloat)));
-    
-    glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 9 * sizeof(GLfloat), (void*) (3 * sizeof(GLfloat)));
-
-    glEnableVertexAttribArray(2);
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 9 * sizeof(GLfloat), (void*) (7 * sizeof(GLfloat)));
-
-    // SECTION: Shaders
-
+int ft_init_opengl(void) {
     GLuint sh_vert = glCreateShader(GL_VERTEX_SHADER);
     GLuint sh_frag = glCreateShader(GL_FRAGMENT_SHADER);
 
@@ -137,74 +176,64 @@ int main(int argc, const char* argv[]) {
     glDeleteShader(sh_frag);
     glUseProgram(sh_prog);
 
-    // SECTION: Textures
+    CORE.sh_prog = sh_prog;
 
-    t_tex2d capture_texture = ft_tex2d_screen_capture(capture_width, capture_height, capture_data); 
-
-	int exit = 0;
-	while(!exit) {
-		SDL_Event event = { 0 };
-		while(SDL_PollEvent(&event)) {
-			switch(event.type) {
-				case SDL_QUIT: {
-					exit = 1;
-				} break;
-
-                case SDL_KEYDOWN: {
-                    if(event.key.keysym.scancode == SDL_SCANCODE_ESCAPE) {
-                        exit = 1;
-                    }
-                }
-			}
-		}
-
-		glClear(GL_COLOR_BUFFER_BIT);
-		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-        
-        glUseProgram(sh_prog);
-        glBindVertexArray(vert_arr);
-        glBindTextureUnit(0, capture_texture.id);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
-		SDL_GL_SwapWindow(window);
-	}
-
-    free(capture_data);
-
-    glDeleteVertexArrays(1, &vert_arr);
-    glDeleteBuffers(1, &vert_buf);
-    glDeleteBuffers(1, &elem_buf);
-    glDeleteTextures(1, &capture_texture.id);
-    glDeleteProgram(sh_prog);
-
-	SDL_GL_DeleteContext(context);
-	SDL_DestroyWindow(window);
-
-	SDL_Quit();
-
-	return 0;
+    return 1;
 }
 
+int ft_poll_events(void) {
+	SDL_Event event = { 0 };
+    while(SDL_PollEvent(&event)) {
+		switch(event.type) {
+			case SDL_QUIT: {
+				CORE.exit = 1;
+			} break;
 
-char* ft_screen_capture(int* w, int* h) {
+            case SDL_KEYDOWN: {
+                CORE.exit = 1;
+            }
+		}
+	}
+
+    return 1;
+}
+
+int ft_should_quit(void) {
+    return CORE.exit;
+}
+
+int ft_display(void) {
+	SDL_GL_SwapWindow(CORE.window);
+
+    return 1;
+}
+
+int ft_quit(void) {
+    glDeleteProgram(CORE.sh_prog);
+
+    SDL_GL_DeleteContext(CORE.context);
+	SDL_DestroyWindow(CORE.window);
+
+	SDL_Quit();
+    return 1;
+}
+
+char* ft_screen_capture(int w, int h) {
     Display* x_display = XOpenDisplay(NULL);
     Window x_root = DefaultRootWindow(x_display);
 
     XWindowAttributes x_window_attrib = { 0 };
     XGetWindowAttributes(x_display, x_root, &x_window_attrib);
 
-    *w = x_window_attrib.width;
-    *h = x_window_attrib.height;
-
     XImage* x_image = XGetImage(
         x_display, x_root,
         0, 0,
-        *w, *h,
+        w, h,
         AllPlanes,
         ZPixmap
     );
 
-    char* data = (unsigned char*) calloc(*w * *h * 4, sizeof(char));
+    char* data = (unsigned char*) calloc(w * h * 4, sizeof(char));
     if(!data) {
         fprintf(stderr, "[ ERR ] X11: %s\n", strerror(errno));
 
@@ -214,16 +243,12 @@ char* ft_screen_capture(int* w, int* h) {
         return NULL;
     }
 
-    for(int i = 0; i < *w * *h * 4; i += 4) {
+    for(int i = 0; i < w * h * 4; i += 4) {
         data[i + 0] = x_image->data[i + 2];
         data[i + 1] = x_image->data[i + 1];
         data[i + 2] = x_image->data[i + 0];
         data[i + 3] = x_image->data[i + 3];
     }
-
-    fprintf(stdout, "[ INFO ] X11: Captured the screen\n");
-    fprintf(stdout, "[ INFO ] X11: Width: %i\n", *w);
-    fprintf(stdout, "[ INFO ] X11: Height: %i\n", *h);
 
     XDestroyImage(x_image);
     XCloseDisplay(x_display);
@@ -231,7 +256,7 @@ char* ft_screen_capture(int* w, int* h) {
     return data;
 }
 
-t_tex2d ft_tex2d_screen_capture(int w, int h, char* data) {
+t_tex2d ft_tex2d(int w, int h, char* data) {
     t_tex2d tex = { 
         .w = w,
         .h = h,
@@ -260,10 +285,106 @@ t_tex2d ft_tex2d_screen_capture(int w, int h, char* data) {
     return tex;
 }
 
-int ft_draw_tex2d(t_tex2d tex, float position[2], float size[2]) {
+int ft_draw_rect(vec2 position, vec2 size, vec4 color) {
+    GLfloat vertices[] = {
+        position[0], position[1], 0.0f,     color[0], color[1], color[2], color[3],     0.0f, 0.0f,     0.0f,
+        position[0] + size[1], position[1], 0.0f,     color[0], color[1], color[2], color[3],     1.0f, 0.0f,   0.0f,
+        position[0], position[1] + size[1], 0.0f,     color[0], color[1], color[2], color[3],     0.0f, 1.0f,   0.0f,
+        position[0] + size[0], position[1] + size[1], 0.0f,     color[0], color[1], color[2], color[3],     1.0f, 1.0f,     0.0f,
+    };
+
+    GLuint indices[] = {
+        0, 1, 2,
+        1, 2, 3
+    };
+
+    GLuint vert_arr;
+    glGenVertexArrays(1, &vert_arr);
+    glBindVertexArray(vert_arr);
+
+    GLuint vert_buf;
+    glGenBuffers(1, &vert_buf);
+    glBindBuffer(GL_ARRAY_BUFFER, vert_buf);
+
+    GLuint elem_buf;
+    glGenBuffers(1, &elem_buf);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elem_buf);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 10 * sizeof(GLfloat), (void*) (0 * sizeof(GLfloat)));
+    
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 10 * sizeof(GLfloat), (void*) (3 * sizeof(GLfloat)));
+
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 10 * sizeof(GLfloat), (void*) (7 * sizeof(GLfloat)));
+ 
+    glEnableVertexAttribArray(3);
+    glVertexAttribPointer(3, 1, GL_FLOAT, GL_FALSE, 10 * sizeof(GLfloat), (void*) (9 * sizeof(GLfloat)));
+ 
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+    glDeleteBuffers(1, &vert_buf);
+    glDeleteBuffers(1, &elem_buf);
+    glDeleteVertexArrays(1, &vert_arr);
+
+    return 1;
+}
+
+int ft_draw_tex2d(t_tex2d tex, vec2 position, vec2 size) {
     // TODO: Implement a texture drawing function
     // I wonder if I should use a global OpenGL objects or local
     // Will think about that tomorrow, gn o/
+    GLfloat vertices[] = {
+        position[0], position[1], 0.0f,     1.0f, 1.0f, 1.0f, 1.0f,     0.0f, 0.0f,     tex.id,
+        position[0] + size[0], position[1], 0.0f,     1.0f, 1.0f, 1.0f, 1.0f,     1.0f, 0.0f,   tex.id,
+        position[0], position[1] + size[1], 0.0f,     1.0f, 1.0f, 1.0f, 1.0f,     0.0f, 1.0f,   tex.id,
+        position[0] + size[0], position[1] + size[1], 0.0f,     1.0f, 1.0f, 1.0f, 1.0f,     1.0f, 1.0f,     tex.id,
+    };
+
+    GLuint indices[] = {
+        0, 1, 2,
+        1, 2, 3
+    };
+
+    GLuint vert_arr;
+    glGenVertexArrays(1, &vert_arr);
+    glBindVertexArray(vert_arr);
+
+    GLuint vert_buf;
+    glGenBuffers(1, &vert_buf);
+    glBindBuffer(GL_ARRAY_BUFFER, vert_buf);
+
+    GLuint elem_buf;
+    glGenBuffers(1, &elem_buf);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elem_buf);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 10 * sizeof(GLfloat), (void*) (0 * sizeof(GLfloat)));
+    
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 10 * sizeof(GLfloat), (void*) (3 * sizeof(GLfloat)));
+
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 10 * sizeof(GLfloat), (void*) (7 * sizeof(GLfloat)));
+ 
+    glEnableVertexAttribArray(3);
+    glVertexAttribPointer(3, 1, GL_FLOAT, GL_FALSE, 10 * sizeof(GLfloat), (void*) (9 * sizeof(GLfloat)));
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, tex.id);
+
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+    glDeleteBuffers(1, &vert_buf);
+    glDeleteBuffers(1, &elem_buf);
+    glDeleteVertexArrays(1, &vert_arr);
 
     return 1;
 }
